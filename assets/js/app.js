1789c720dce0c253760fa65b5f393679c4f6a26e
mallano <maelallano1@gmail.com>
2018-04-11 23:02:40 +0200

    jonathanSection done ?

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ let $ = require('jquery');
 /* ----------- start function scrollTo jquery ---------*/
 let scrollTo = (button, target, speed) => {
 	$(button).click(function (){
-		let goTo = $(target).offset().top - 100;
+		let goTo = $(target).offset().top - 60;
         $('html, body').animate({
             scrollTop: goTo
         }, speed);
@@ -12,10 +12,8 @@ let scrollTo = (button, target, speed) => {
 };
 
 $(document).ready(function (){
-    // scrollTo("#click1", "#div1", 2000);
-    // scrollTo("#click2", "#div2", 1000);
+    scrollTo("#Layer_1", ".Home", 1000);
     scrollTo(".header-navBarList-item1", ".PortalGun-section", 1000);
-    scrollTo("#Layer_1", "#div1", 1000);
 		scrollTo(".header-navBarList-item2", ".Escape-section", 1000);
 		scrollTo(".header-navBarList-item3", ".Escape-section__registration__title", 1000);
 
@@ -53,19 +51,19 @@ setTimeout(() => {
 let scaleHeaderText = () => {
   let headerText = document.querySelectorAll(".Home-headerText");
 
-  if (document.body.scrollTop > 0 && document.body.scrollTop < 100) {
-    let firstFont = headerText[0].style.fontSize + Math.floor(document.body.scrollTop);
-    headerText[0].style.fontSize = Math.floor(document.body.scrollTop) / 2 + "px";
+  if (window.scrollY > 0 && window.scrollY < 100) {
+    let firstFont = headerText[0].style.fontSize + Math.floor(window.scrollY);
+    headerText[0].style.fontSize = Math.floor(window.scrollY) / 2 + "px";
   }
-  if (document.body.scrollTop > 50 && document.body.scrollTop < 150) {
-    let firstFont = headerText[1].style.fontSize + Math.floor(document.body.scrollTop);
-    headerText[1].style.fontSize = (Math.floor(document.body.scrollTop) - 50) / 2 + "px";
+  if (window.scrollY > 50 && window.scrollY < 150) {
+    let firstFont = headerText[1].style.fontSize + Math.floor(window.scrollY);
+    headerText[1].style.fontSize = (Math.floor(window.scrollY) - 50) / 2 + "px";
   }
-  if (document.body.scrollTop > 100 && document.body.scrollTop < 200) {
-    let firstFont = headerText[2].style.fontSize + Math.floor(document.body.scrollTop);
-    headerText[2].style.fontSize = (Math.floor(document.body.scrollTop) - 100) / 2 + "px";
+  if (window.scrollY > 100 && window.scrollY < 200) {
+    let firstFont = headerText[2].style.fontSize + Math.floor(window.scrollY);
+    headerText[2].style.fontSize = (Math.floor(window.scrollY) - 100) / 2 + "px";
   }
-  if (document.body.scrollTop === 0) {
+  if (window.scrollY === 0) {
     for (let i = 0; i < 3; i++)
     headerText[i].style.display = "none";
   }
@@ -75,4 +73,6 @@ let scaleHeaderText = () => {
   }
 };
 
-document.body.addEventListener('scroll', scaleHeaderText());
+document.addEventListener('scroll', () => {
+  scaleHeaderText();
+});
