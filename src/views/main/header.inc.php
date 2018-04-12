0068224ebeb3d6ed79699b490af792509cec293f
mallano <maelallano1@gmail.com>
2018-04-12 12:37:14 +0200

    favicon

--- src/views/main/header.inc.php
+++ src/views/main/header.inc.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title><?=SITE_NAME?></title>
-    <link rel="shortcut icon" href="./favicon.ico">
+    <link rel="shortcut icon" href="./images/faviconAperture.png">
     <link rel="stylesheet" href="<?=WEBROOT?>build/app.css">
 </head>
 <body>
